e2383500619e87afc6ed30f685bc1c8ad8dd3180
kcsandyy <kemboisandra20@gmail.com>
2023-11-15 04:17:07 -0800

    Prints a string

---
+++ _puts.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
