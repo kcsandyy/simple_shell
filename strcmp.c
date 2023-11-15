0a84bbadb506fb64d32a20dcc5643bf8e1b6378b
kcsandyy <kemboisandra20@gmail.com>
2023-11-15 04:17:38 -0800

    Compares two strings

---
+++ strcmp.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string to be compared
+ * @s2: string to be compared
+ * Return: 0 if sucessful and any other number if not
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+	return (*s1 - *s2);
+	}
+}
